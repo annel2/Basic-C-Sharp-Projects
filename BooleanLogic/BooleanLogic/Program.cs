3136d48eafae858e59e4f931e59eaa93d621ae03
Anne <saigonmafia228@gmail.com>
2018-12-19 23:01:41 -0800

    good

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTickets = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool qualified = (age > 15 && DUI == false && speedingTickets <= 3);
+            Console.WriteLine(qualified);
+
+            Console.ReadLine();
+                
+
+        }
+   }
+}
