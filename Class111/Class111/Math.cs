de651aad6dc875dcba2bf00bd9ac9586b2c2b1e7
Anne <saigonmafia228@gmail.com>
2019-01-11 15:04:02 -0800

    yay

---
+++ Class111/Class111/Math.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class111
+{
+    class Math
+    {
+        public static int MultiplybyTwo(int numberOne)
+        {
+            int product = numberOne * 2;
+
+            return product;
+
+        }
+        public static int DividebyTwo(int numberOne)
+        {
+
+            int quotient = numberOne / 2;
+
+            return quotient;
+        }
+        public static int AddTwo(int numberOne)
+        {
+
+            int sum = numberOne + 2;
+
+            return sum;
+        }
+
+    }
+}
---
+++ Class111/Class111/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            int product = Math.MultiplybyTwo(numberOne);
+            Console.WriteLine(numberOne + " multiply by two" + " equals " + product);
+            Console.ReadLine();
+
+
+            int quotient = Math.DividebyTwo(numberOne);
+            Console.WriteLine(numberOne + " divide by two" + " equals " + quotient);
+            Console.ReadLine();
+           
+            int sum = Math.AddTwo(numberOne);
+            Console.WriteLine(numberOne + " add two" + " equals " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
