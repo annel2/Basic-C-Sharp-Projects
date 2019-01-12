a25d2f6ac2c077c5be016a7d3ecf5f948020bbf9
Anne <saigonmafia228@gmail.com>
2019-01-11 21:55:01 -0800

    finish

---
+++ ClassMethods117/ClassMethods117/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods117
+{
+    class Math
+    {
+        public void Division(int x)
+        {
+            int quotient = x / 2;
+            Console.WriteLine(quotient);
+
+        }
+        //Create a method with output parameters.
+        public static void Sum(out int x)
+        {
+            x = 5 + 1;
+
+        }
+        //Overload a method.
+        public static void Sum(int y, out int x)
+        {
+            x = 5 + 1;
+
+        }
+    }
+}
---
+++ ClassMethods117/ClassMethods117/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethods117
+{
+    class Program
+    {
+        //Declare a class to be static.
+        static void Main(string[] args)
+        {
+            Math Puppy = new Math();
+            Console.WriteLine("Please input a number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Puppy.Division(x);
+
+            Math.Sum(out x);
+
+            Console.ReadLine();
+        }
+    }
+}
