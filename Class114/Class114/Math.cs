cdfed1ea4c39ea3e4820e1d6cf5e050a65a9778a
Anne <saigonmafia228@gmail.com>
2019-01-11 17:42:36 -0800

    final

---
+++ Class114/Class114/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class114
+{
+    class Math
+    {
+        public static int SubtractThree(int x = 8)
+        {
+            int difference = x - 3;
+            return difference;
+
+        }
+        public static decimal SubtractThree(decimal x = 8.5m)
+        {
+            decimal product = x * 10;
+            return product;
+        }
+        public static string SubtractThree(string x = "five")
+        {
+            int y = Convert.ToInt32(x);
+            int sum = y + 1;
+            return sum.ToString();
+        }
+
+}
+}
---
+++ Class114/Class114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Peony = new Math();
+            //int result = Math.SubtractThree(x: 8);
+            Console.WriteLine(Math.SubtractThree(x: 8));
+            Console.ReadLine();
+
+            Math Dahlia = new Math();
+            Console.WriteLine(Math.SubtractThree(x: 8.5m));
+            Console.ReadLine();
+
+            Math Tulip = new Math();
+            Console.WriteLine(Math.SubtractThree(x: "5"));
+            Console.ReadLine();
+
+
+        }
+    }
+}
