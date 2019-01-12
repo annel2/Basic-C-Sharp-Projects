c190b74022ac5c462e58d2668ed42a3b0b646a7f
Anne <saigonmafia228@gmail.com>
2019-01-11 19:26:00 -0800

    complete

---
+++ Class115/Class115/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class115
+{
+    class Math
+    {
+        public static int Multiplication(int x = 10, int y = 20)
+        {
+
+                  int product = x * y;
+            
+                  return product;
+        }
+
+    }
+}
+;
---
+++ Class115/Class115/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math Kitty = new Math();
+            Console.WriteLine("Please enter the first number.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second number as an option.");
+            string z = Console.ReadLine();
+            Console.WriteLine("The product of your input(s) equals:");
+            if (z != "")
+            {
+                int y = Convert.ToInt32(z);
+                Console.WriteLine(Math.Multiplication(x,y));
+            }
+            else
+            {
+                Console.WriteLine(Math.Multiplication(x));
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
