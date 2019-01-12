82d01bcc19682e9aff80330d8646f0665dc56814
Anne <saigonmafia228@gmail.com>
2019-01-11 16:21:32 -0800

    done

---
+++ Class113/Class113/Math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class113
+{
+    class Math
+    {
+        //Have the method do a math operation on the first integer and display the second integer to the screen.
+        public static void MultiplybyFive (int x=5, int y=10)
+        {
+            int product = 5 * x;
+            Console.WriteLine(y);
+        }
+
+    }
+}
---
+++ Class113/Class113/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate the class.
+            Math zebra = new Math();
+
+            //Call the method in the class, passing in two numbers.
+            Math.MultiplybyFive(5, 10);
+
+            //Call the method in the class, specifying the parameters by name.
+            Math.MultiplybyFive(x: 5, y: 10);
+            Console.ReadLine();
+        }
+    }
+}
