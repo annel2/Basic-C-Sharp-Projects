ebbb151b161b5576de7e8031f0ed08ea24299b81
Anne <saigonmafia228@gmail.com>
2019-01-13 16:44:38 -0800

    yes

---
+++ Inheritance120/Inheritance120/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance120
+{
+    class Person
+
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + LastName);
+            Console.WriteLine("Name: " + FirstName + " " + LastName );
+        }
+
+
+
+    }
+}
---
+++ Inheritance120/Inheritance120/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee();
+            Employee1.FirstName = "Sample";
+            Employee1.LastName = "Student";
+            Employee1.SayName();
+            
+            Console.ReadLine();
+        }
+    }
+}
