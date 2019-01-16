eed95229e4c2d033f5cc87e32b431b31a4b6002e
Anne <saigonmafia228@gmail.com>
2019-01-15 17:52:46 -0800

    finished

---
+++ Enum134/Enum134/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            public enum DaysOfTheWeek
+            {
+            Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+
+            }
+            Console.WriteLine("Please enter in the current day of the week.");
+        }   enum DaysoftheWeek = day;
+            }
+            Catch(Exception ex)
+            {
+            Console.WriteLine("Please enter an actual day of the week.");
+            } 
+
+
+    }Console.ReadLine();
+}
+
---
+++ Struct137/Struct137/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct137
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. In the Main() method, create an object of data type Number and assign an amount to it.
+            Number number1 = new Number();
+            number1.Amount = 5000m;
+
+            //3. Print this amount to the console.
+            Console.WriteLine(number1.Amount);
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+}
