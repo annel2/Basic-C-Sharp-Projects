9967252de728aa998f8f1b718f36e740400e9265
Anne <saigonmafia228@gmail.com>
2019-01-18 20:17:54 -0800

    done

---
+++ FileHandling154/FileHandling154/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandling154
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Ask a user for a number.
+            Console.WriteLine("Please enter a number.");
+            string text = Convert.ToString(Console.ReadLine());
+
+
+            ////2. Log that number to a text file.
+            File.WriteAllText("C:\\Sample\\log.txt", text);
+
+            ////3. Print the text file back to the user.
+            text = File.ReadAllText("C:\\Sample\\log.txt");
+            Console.WriteLine(text);
+            Console.ReadLine();
+        }        
+
+    }
+
+}
