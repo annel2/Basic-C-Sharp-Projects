63a900541f12d7a6157b81e56c267c0520849a56
Anne <saigonmafia228@gmail.com>
2019-01-20 19:07:22 -0800

    complete

---
+++ AdditionalFeatures160/AdditionalFeatures160/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures160
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Create a const variable.
+            const string name = "The Tech Academy";
+            Console.WriteLine("Welcome to {0}. How can we help you?", name);
+
+            // Create a variable using the keyword "var".
+            var i = 10;
+            Console.WriteLine(i);
+            Console.ReadLine();
+
+
+        }
+
+    }
+
+
+}
---
+++ AdditionalFeatures160/AdditionalFeatures160/SpringFlowers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeatures160
+{
+    class SpringFlowers
+    {
+        //public string Name { get; set; }
+        //public string Height { get; set; }
+        //public string SunlightNeeds { get; set; }
+        //public int WaterAmount { get; set; }
+        //public string Notes { get; set; }
+
+        //Chain two constructors together.
+        public SpringFlowers(string tulips, string daffodils)
+        {
+            string nameOfFlowers = tulips + daffodils;
+        }
+
+        public SpringFlowers(string tulips) : this(tulips, "")
+        {
+            string nameOfFlowers = tulips;
+        }
+       
+    }
+}
